70ba72580547eff75b2f0ddc81cb28e23e54c31e
zacharyestrin9 <zacharyestrin9@gmail.com>
2026-03-05 00:45:43 +0000

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App=({})=>{
     <Helmet>
       <link rel="icon" type="image/svg+xml" href="/vite.svg" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>NJSCD - {pageTitle}</title> 
+      <title>{`NJSCD - ${pageTitle}`}</title> 
       {/* TODO: implement naming with jotai atom */}
     </Helmet>
     <motion.div className='AppWrapper'>
